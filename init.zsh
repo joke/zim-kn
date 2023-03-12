cc0b9e2744aa8dc2a2ea5eb3d44ee4a4856c8a50
Joke de Buhr <joke@xckk.de>
2023-03-12 16:14:25 +0100

    fix: new version detection message
    Signed-off-by: Joke de Buhr <joke@xckk.de>

--- init.zsh
+++ init.zsh
@@ -6,6 +6,6 @@
   local compfile=$1/functions/_kn
   if [[ ! -e $compfile || $compfile -ot $command ]]; then
     $command completion zsh >| $compfile
-    print -u2 -PR "* Detected a new version 'kn'. Regenerated completions.
+    print -u2 -PR "* Detected a new version 'kn'. Regenerated completions."
   fi
 } ${0:h}
